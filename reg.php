ee96b0b741eb72c1706af8098d4708e56e52df23
Daniill Mikula <86491030+mdanil00@users.noreply.github.com>
2021-06-30 15:07:05 +0700

    Add files via upload

---
+++ reg.php
@@ -0,0 +1,13 @@
+<?php
+    $login = filter_var (trim($_POST['login']), FILTER_SANITIZE_STRING);
+    $pass = filter_var (trim($_POST['pass']), FILTER_SANITIZE_STRING);
+    $name = filter_var (trim($_POST['name']), FILTER_SANITIZE_STRING);
+   
+
+
+    $mysql = new mysqli('localhost', 'root', '', 'register_bd');
+    $mysql->query("INSERT INTO `users` (`login`, `pass`, `name`)
+VALUES('$login', '$pass', '$name')");
+    $mysql->close();
+/*header('Location: /project');*/
+?>
